d8909181b7af7d3b7b227021137ee4e8f414a42e
Siomara Murta <siomara.murta@gmail.com>
2025-01-17 15:05:16 -0300

    Aplicando correções no arquivo loginPage

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -2,30 +2,11 @@
       visit() {
         cy.visit('/entrar');
         }
-  
-      fillLogin(login) {
-        cy.get('#username').type(login);
-      }
-  
-      fillPassword(password) {
-        cy.get('#password').type(password);
-      }
-  
-      submitLogin() {
-        cy.get('button.btn-primary--solid')
-          .contains('Entrar')
-          .click();
-     }      
 
       checkModalMessage(expectedMessage) {
         cy.get('.cdk-overlay-pane.modal-panel-overlay', { timeout: 10000 })
           .should('be.visible')
           .contains(expectedMessage);
-      }
-    
-      checkErrorMessage(message) {
-        cy.get('p.form-error-message', { timeout: 10000 })
-        .contains(message);
       }
 
       checkFieldErrorMessage(message) {
